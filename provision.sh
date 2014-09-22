c327f84f143f583788964a5b0162f1f9ea44ef27
Pal Szak <pal_szak@epam.com>
2014-09-22 13:13:46 +0200

    Clean and fix provision

--- provision.sh
+++ provision.sh
@@ -3,14 +3,13 @@ apt-get -y install curl git
 curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3 --auto-dotfiles
 source /usr/local/rvm/scripts/rvm
 gem install bundler
-gem install rerun
 curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install 0.10.32
 nvm use 0.10.32
 curl -L https://npmjs.org/install.sh | sh
 npm install -g bower
-npm install -g forever
+cd /vagrant
 bundle install
 bower install --allow-root
 nohup ruby /vagrant/gitHubStatusEditor.rb > /dev/null 2>&1 &
